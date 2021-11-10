92ac6459658dd25ece6c37253a586da41f062fb9
AlexandrFedorow <75899441+AlexandrFedorow@users.noreply.github.com>
2021-11-10 23:52:55 +0300

    visits ctr

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
-
+import config
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    config.ctr += 1
+    return render_template('main.html', ctr=config.ctr)
+
 
 
 if __name__ == "__main__":
