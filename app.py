a7e9a0be05ddac0d6e0491e7a833d0f3e4b20ff4
AlexandrFedorow <75899441+AlexandrFedorow@users.noreply.github.com>
2021-11-29 22:31:41 +0300

    user counter

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template
-import config
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Count(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    n = db.Column(db.Integer(), primary_key=False)
+
+    def __repr__(self):
+        return '<Count %r' % self.id
 
 
 @app.route('/')
 def main():
-    config.ctr += 1
-    return render_template('main.html', ctr=config.ctr)
-
+    line = Count.query.filter_by(id=1).all()
+    line[0].n += 1
+    db.session.commit()
+    nline = Count.query.filter_by(id=1).all()
+    return render_template('main.html', ctr=nline[0].n)
 
 
 if __name__ == "__main__":
